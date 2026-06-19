12e865feda70ed1c3468825c2fd3c5c68eab7eaf
Nicolás Barlasina <nicolas.barlasina@wetcom.com>
2026-06-19 12:31:43 -0300

    chore: Actualizar URL de WebApp en Config.js

--- config/Config.js
+++ config/Config.js
@@ -17,7 +17,7 @@ const Config = {
 
   // Configuración de WebApp para Generador de Borradores
   // IMPORTANTE: Deberás reemplazar este valor tras hacer la publicación web.
-  WEB_APP_URL: "URL_DE_LA_WEBAPP_AQUI",
+  WEB_APP_URL: "https://script.google.com/a/macros/wetcom.com/s/AKfycbw-ZHuJoFYN4hhOB-GJPwegSIjsbh5VURmTyFTc5_zOKG1WUIQE-IdxIyuMXRZNYYY/exec",
 
   /**
    * Obtiene una propiedad de script de forma segura.
